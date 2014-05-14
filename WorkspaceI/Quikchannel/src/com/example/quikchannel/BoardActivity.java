d12cc87da157f3435f24842dc35fbd82cc3d0754
Will <wpapsco@gmail.com>
2014-05-13 18:45:23 -0700

    adding this new branch

---
+++ WorkspaceI/Quikchannel/src/com/example/quikchannel/BoardActivity.java
@@ -0,0 +1,14 @@
+package com.example.quikchannel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BoardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_board);
+	}
+}
