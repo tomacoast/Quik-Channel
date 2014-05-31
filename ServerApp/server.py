27e0a2857f1b0b4154007c7693b8c0b7dd75f68c
Will <wpapsco@gmail.com>
2014-05-31 10:54:30 -0700

    app and server update

--- ServerApp/server.py
+++ ServerApp/server.py
@@ -3,12 +3,14 @@ import time
 import thread
 import datetime
 import socket
+import sys
+from PIL import Image
 
 class Board:
 	threads = []
 	def __init__(self, title):
 		self.title = title
-		self.json_file = open("board.json", "w")
+		#self.json_file = open("board.json", "w"
 		self.post_id_counter = 0
 		#self.json_file_data = simplejson.loads(self.json_file)
 	
@@ -17,22 +19,29 @@ class Board:
 		post_id = self.post_id_counter
 		new_thread = Thread(thread_title, thread_text, thread_author, post_id)
 		self.threads.append(new_thread)
+		return post_id
 	
 	def add_post(self, thread, title, text, author):
 		self.post_id_counter = self.post_id_counter + 1
 		post_id = self.post_id_counter
 		new_post = Post(title, text, author, post_id) 
 		thread.add_post(new_post)
+		return new_post
 	
 	def update(self):
+		self.json_file = open("board.json", "w")
 		threads_in_json = []
 		for thread in self.threads:
 			thread.update()
 			threads_in_json.append(thread.in_json)
-		board_data = {"title":self.title, "threads":threads_in_json}
+		board_data = {"title":self.title, "threads":threads_in_json, "id_counter":self.post_id_counter}
 		self.in_json = json.dumps(board_data, sort_keys=False, indent=2)
 		for line in self.in_json:
 			self.json_file.write(line)
+		self.json_file.close()
+		
+	def set_id(self, id_num):
+		self.post_id_counter = id_num
 
 class Thread:
 
@@ -82,16 +91,35 @@ class Post:
 
 class JSON_Board_Loader:
 	
-	def add_to_loaded(self, json_file_path, board):
+	#Adds received json to board
+	def add_to_loaded(self, data, board):
 		self.board = board
-		self.json_data = open(json_file_path)
-		data = json.load(self.json_data)
+		#self.json_data = open(json_file_path)
+		#data = json.load(self.json_data)
 		if data["threadid"] == -1:
-			board.add_thread(data["title"], data["text"], data["author"])
+			return board.add_thread(data["title"], data["text"], data["author"])
 		else:
 			for thread in board.threads:
 				if thread.thread_id == data["threadid"]:
-					board.add_post(thread, data["title"], data["text"], data["author"])
+					return board.add_post(thread, data["title"], data["text"], data["author"]).post_id
+	
+	#Loads board if server ever stopped
+	def load_backup(self, backup_path):
+		json_file = open(backup_path)
+		data = json.load(json_file)
+		board = Board(data["title"])
+		for thread in data["threads"]:
+			new_thread = Thread(thread["entries"][0]["title"], thread["entries"][0]["text"], thread["entries"][0]["author"], thread["entries"][0]["id"])
+			board.threads.append(new_thread)
+			for entry in thread["entries"]:
+				if entry["id"] == thread["entries"][0]["id"]:
+					pass
+				else:
+					new_post = Post(entry["title"], entry["text"], entry["author"], entry["id"])
+					new_thread.add_post(new_post)
+		board.set_id(data["id_counter"])
+		return board
+		
 
 def recvall(sock):
 	data = ""
@@ -117,16 +145,24 @@ def recvUntilEOI(sock):
 	while part != "":
 		part = sock.recv(4096)
 		data+=part
-		for i in range(1, len(part)):
-			if ord(part[i]) == 217 and ord(part[i-1]) == 255:
+		for i in range(1, len(data)):
+			if ord(data[i]) == 217 and ord(data[i-1]) == 255:
 				return data[0:len(data)-2] #removing the EOI bits in cas it's not an image
 	return data
 
 def writeToFile(data, filename):
 	f = open(filename, "wb")
 	f.write(data)
+	f.close()
 
-def serveClient(client, address):
+	#now make a resized version
+	f = open(filename + '.thum', 'wb')
+	im = Image.open(filename)
+	im.thumbnail((128, 128))
+	im.save(f, "JPEG")
+
+
+def serveClient(client, address, board):
 	data = recvUntilEOI(client) #getting the initial data
 	if data:
 		jsondata = data
@@ -140,39 +176,48 @@ def serveClient(client, address):
 		if imgdata != None:
 			imgdata = imgdata + (chr(255) + chr(217)) #adding the EOI bits back to the image
 		loadedData = json.loads(jsondata)
+		print jsondata
 		print "found json data :" + str(loadedData)
 		if loadedData['request'] == False: #if it's not a request
-			if (loadedData['threadid'] >= 0): #adding a post to a thread
-				#write the image to a file with the name of the post id if the image is there
-				if imgdata:
-					print "saving an image to " + str(loadedData['threadid']) + ".jpg"
-					writeToFile(imgdata, str(loadedData['threadid']) + ".jpg")
-				#TODO: write the json to a post
-			else: #TODO: adding a thread to a board and save the image with the correct ID
-				pass
+			#write the image to a file with the name of the post id if the image is there
+			loader =JSON_Board_Loader()
+			post = loader.add_to_loaded(loadedData, board)
+			if imgdata:
+				print "saving an image to " + str(post) + ".jpg"
+				writeToFile(imgdata, str(post) + ".jpg")
+			#writes the json to a post
+			board.update()
 		else: #send requested data to the client
+			board.update()
 			if loadedData['request'] == 'board':
-				client.send('data!\n') #TODO: send json data
+				client.send(board.in_json) #send json data
 			if loadedData['request'] == 'image':
-				img = open(loadedData['image']) #send the image
-				client.send(img.read())
-				img.close()
+				try:
+					img = open(loadedData['image'], 'r') #send the image
+					client.send(img.read())
+					print 'sent data about image ' + loadedData['image']
+					img.close()
+				except IOError:
+					print 'no image \"' + loadedData['image'] + '\"'
 
 	client.close()
 	print "client closed!"
 
-# loader = JSON_Board_Loader()
-# manChan = Board("manChan")
-# manChan.add_thread("Sauce?", None, None)
-# manChan.add_post(manChan.threads[0], "Why I Hate QuikChan", "Does nobody ever post anything goohd?", None)
-# manChan.add_post(manChan.threads[0], None, "This thread blows", "Weston")
-# loader.add_to_loaded("new_thread_temp.json", manChan)
-# loader.add_to_loaded("new_post_temp.json", manChan)
-# manChan.update()
-# print manChan.in_json
-
+loader = JSON_Board_Loader()
+board_file = open("board.json", "r+")
+board_file.seek(0)
+if board_file.read(1) == '{':
+	board = loader.load_backup("board.json")
+else:	
+	board = Board("board")
+	board.add_thread("First Thread", "Board created successfully", "Admin")
+board_file.close()
+board.update()
 host = ''
-port = 42083
+port = 8123
+if (len(sys.argv) > 1):
+	port = int(sys.argv[1])
+print port
 backlog = 5
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.bind((host,port)) 
@@ -180,4 +225,4 @@ s.listen(backlog)
 while 1:
 	client, address = s.accept() 
 	print "client recieved at " + address[0]
-	thread.start_new_thread(serveClient, (client, address))
+	thread.start_new_thread(serveClient, (client, address, board))
--- WorkspaceI/Quikchannel/src/com/example/quikchannel/Views/EntryView.java
+++ WorkspaceI/Quikchannel/src/com/example/quikchannel/Views/EntryView.java
@@ -13,7 +13,6 @@ import android.graphics.Rect;
 import android.text.SpannableString;
 import android.text.style.AlignmentSpan;
 import android.text.style.ForegroundColorSpan;
-import android.text.util.Linkify;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -61,7 +60,6 @@ public class EntryView extends RelativeLayout {
 		contentView.setLayoutParams(p);
 		contentView.setTextColor(getContext().getResources().getColor(R.color.text_color));
 		contentView.setPadding(10, 10, 10, 10);
-		contentView.setAutoLinkMask(Linkify.WEB_URLS);
 		this.addView(contentView);
 		
 		boxPaint = new Paint();
